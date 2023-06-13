a915b117acde06437288b93dbb0dc075804d13f7
catkowalski25 <risha25@list.ru>
2023-06-13 23:04:41 +0300

    lesson 2 (part 1)

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,13 @@
 function getArrayParams(...arr) {
-
+  let sum = 0;
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    min = arr[i] < min ? arr[i] : min;
+    max = arr[i] > max ? arr[i] : max;
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
