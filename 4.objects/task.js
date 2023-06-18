b926c3946247cbfe8c6789a64b6ff7ed50f41d6a
catkowalski25 <risha25@list.ru>
2023-06-18 21:27:26 +0300

    lesson 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")) { 
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.hasOwnProperty("marks") || this.marks.length === 0) { 
+        return 0;
+    }
+    return this.marks.reduce((acc, mark, index, arr) => {
+        acc += mark;
+        if (index === (arr.length - 1)) {
+            return acc / arr.length;
+        }
+        return acc;
+        }, 0);
+   
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
